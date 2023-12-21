d1c973557145458ddafbb2e21a9d6614bac9748e
mak-454 <ahmed.khan@oneconvergence.com>
2023-12-21 18:24:05 +0000

    simple retrv conv chain using conv buffer with langchain.

--- README.md
+++
@@ -1,2 +0,0 @@
-# rayserve-exps
-Ray serve related experiments
---
+++ conv_bk.py
@@ -0,0 +1,93 @@
+from colorama import Fore
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, MessagesPlaceholder, \
+    HumanMessagePromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.chains.chat_vector_db.prompts import CONDENSE_QUESTION_PROMPT
+from chromadb.utils import embedding_functions
+
+system_template = """Answer the user question using the provided context and chat history. 
+If you cannot find the answer from the pieces of context, just say that you don't know, don't try to make up an answer.
+----------------
+CONTEXT:
+{context}
+
+CHAT HISTORY:
+{chat_history}
+
+USER QUESTION:
+{question}
+
+"""
+
+if __name__ == '__main__':
+    print("status: loading document")
+
+    loader = PyPDFLoader("./selfrag.pdf")
+    pages = loader.load_and_split()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len,
+    )
+
+    docs = text_splitter.split_documents(pages)
+
+    embeddings_model_name = "sentence-transformers/all-MiniLM-L6-v2"
+
+    #default_ef = embedding_functions.DefaultEmbeddingFunction()
+    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+    #embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+    tech_store = Chroma.from_documents(
+        docs, embeddings, collection_name="technology"
+    )
+    print("status: configure llm")
+    llm = ChatOpenAI(
+        model_name="gpt-3.5-turbo",
+        temperature=0,
+        #max_tokens=1024,
+    )
+
+    print("status: confiure chain")
+    chat_memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True, output_key="answer")
+    prompt = ChatPromptTemplate.from_messages([
+        SystemMessagePromptTemplate.from_template(system_template),
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{question}")
+    ])
+
+
+    tech_qa = ConversationalRetrievalChain.from_llm(
+        llm = llm,
+        memory = chat_memory,
+        retriever = tech_store.as_retriever(),
+        verbose = False,
+        combine_docs_chain_kwargs={'prompt': prompt},
+        get_chat_history = lambda h : h
+    )
+
+    """
+    tech_qa = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=tech_store.as_retriever(),
+        verbose=False, condense_question_prompt=prompt,memory = chat_memory
+    )
+    """
+
+    while True:
+        query = input("> ")
+        result = tech_qa({"question": query})
+        print(Fore.YELLOW + result['answer'])
+
+
+
+
